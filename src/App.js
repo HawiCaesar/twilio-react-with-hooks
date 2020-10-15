8d68f42035d81b3cac83a09eb485a780c1d04c4f
Brian Hawi O <brianhawi92@gmail.com>
2020-10-15 13:53:48 +0300

    change css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header>
+        <h1>Video Chat with React Hooks WooW</h1>
       </header>
+      <main>
+        <p>Video Chat goes here</p>
+        <p> Made with <span role="img" aria-label="React">
+            ⚛
+          </span>{' '}
+          by <a href="https://twitter.com/HawiCaesar">Hawi Caesar</a></p>
+      </main>
     </div>
   );
 }
